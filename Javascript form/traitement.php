022a5adc39c46ccc8e714159d6e1e353ddaf5ff0
kevillard <kevin.v@codeur.online>
2017-10-15 19:05:36 +0200

    final

--- Javascript form/traitement.php
+++ Javascript form/traitement.php
@@ -1,22 +1,16 @@
 <?php
 $destinataire = $_POST['email'];
-$expediteur = 'kevin.v@codeur.online';
+$to = 'kevin.v@codeur.online';
+$object = "portfolio message";
 $firstname = $_POST['firstname'];
 $lastname = $_POST['lastname'];
 $codepostal = $_POST['codep'];
 $town = $_POST['town'];
 $headers  = 'MIME-Version: 1.0' . "\n";
 $headers .= 'Content-type: text/html; charset=ISO-8859-1'."\n";
-$headers .= 'Reply-To: '.$expediteur."\n";
-$headers .= 'From: '.$firstname.$lastname.'<'.$expediteur.'>'."\n";
+$headers .= 'From: '.$firstname.$lastname.;
 $headers .= 'Delivered-to: '.$destinataire."\n";    
-$message = '<div style="text-align: center; font-weight: bold"> "Ville: " '.$town.' '<br>' "Code postal: " '.$codepostal.' '<br>' '.$_POST['message'].'</div>';
-if (mail($destinataire, $message, $headers))
-{
-    echo 'Votre message a bien été envoyé ';
-}
-else
-{
-    echo "Votre message n'a pas pu être envoyé";
-}
+$message = '<div style="text-align: center; font-weight: bold"> Ville: '.$town.'Code postal: '.$codepostal. .$_POST['message'].'</div>';
+
+mail($to, $object, $message, $headers);
 ?>
